f293e6c80d2b6012762bfd83260b7da669b4419f
Gábor <Gbor@DESKTOP-0RI5UCJ>
2018-10-02 22:52:59 +0200

    asd

---
+++ BejegyzesProjekt/Bejegyzes.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BejegyzesProjekt
+{
+    class Bejegyzes
+    {
+        private string szerzo;
+        private string tartalom;
+        private int likeok;
+        private DateTime letrejot;
+        private DateTime szerkesztve;
+
+        public Bejegyzes(string szerzo, string tartalom)
+        {
+            this.szerzo = szerzo;
+            this.tartalom = tartalom;
+            this.likeok = 0;
+            this.letrejot = DateTime.Now;
+            this.szerkesztve = letrejot;
+        }
+
+        public string Szerzo
+        {
+            get
+            {
+                return szerzo;
+            }
+
+        }
+
+        public string Tartalom
+        {
+            get
+            {
+                return tartalom;
+            }
+
+            set
+            {
+                tartalom = value;
+                this.szerkesztve = DateTime.Now;
+            }
+
+        }
+
+        public int Likeok
+        {
+            get
+            {
+                return likeok;
+            }
+
+        }
+
+        public DateTime Letrejot
+        {
+            get
+            {
+                return letrejot;
+            }
+
+        }
+
+        public DateTime Szerkesztve
+        {
+            get
+            {
+                return szerkesztve;
+            }
+        }
+        public void Like()
+        {
+            likeok++;
+        }
+        public string Kiir()
+        {
+            return szerzo + "-" + likeok + letrejot + "\n" + "Szerkesztve:" + szerkesztve + "\n" + tartalom;
+        }
+        public override string ToString()
+        {
+            if (letrejot == szerkesztve)
+            {
+                return szerzo + "-" + likeok + "-" + letrejot + "\n" + tartalom;
+            }
+            else
+            {
+                return szerzo + "-" + likeok + letrejot + "\n" + "Szerkesztve:" + szerkesztve + "\n" + tartalom;
+            }
+
+        }
+    }
+}
--- BejegyzesProjekt/Program.cs
+++ BejegyzesProjekt/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,129 @@ namespace BejegyzesProjekt
     {
         static void Main(string[] args)
         {
+            Bejegyzes valami = new Bejegyzes("valami", "xzy");
+
+            /*
+            Console.WriteLine(valami);
+            valami.Tartalom = "asd";
+            Console.WriteLine(valami);
+            Console.ReadKey();
+            */
+
+            List<Bejegyzes> lista = new List<Bejegyzes>();
+            lista.Add(new Bejegyzes("Albert Hoffmann", "LSD"));
+            lista.Add(new Bejegyzes("Dan Millmann", "Szókratész utazásai"));
+            Console.WriteLine("Kérem adja meg hány darab új bejegyzést szeretne felvenni");
+            int db = int.Parse(Console.ReadLine());
+            for (int i = 0; i < db; i++)
+            {
+                Console.WriteLine("Kérem adja meg az {0}.bejegyzés szerzőjét", i + 1);
+                string szerzo = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("Kérem adja meg az {0}.bejegyzés tartalmát", i + 1);
+                string tartalom = Convert.ToString(Console.ReadLine());
+                lista.Add(new Bejegyzes(szerzo, tartalom));
+            }
+
+            Console.WriteLine();
+            StreamReader beolvas = new StreamReader("bejegyzesek.txt");
+            while (!beolvas.EndOfStream)
+            {
+                string sor = beolvas.ReadLine();
+                string[] adatok = sor.Split(';');
+
+                lista.Add(new Bejegyzes(adatok[0], adatok[1]));
+            }
+            beolvas.Close();
+
+            int likeszorzo = lista.Count * 20;
+            Random rnd = new Random();
+            for (int i = 0; i < likeszorzo; i++)
+            {
+                int likeotkapo = rnd.Next(lista.Count);
+                lista[likeotkapo].Like();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Modositsa a 2. bejegyzést");
+            lista[1].Tartalom = Console.ReadLine();
+            Console.WriteLine();
+            for (int i = 0; i < lista.Count; i++)
+            {
+                Console.WriteLine(lista[i]);
+            }
+
+            Bejegyzes max;
+            max = lista[0];
+            for (int i = 1; i < lista.Count; i++)
+            {
+                Bejegyzes aktual = lista[i];
+
+                if (max.Likeok < aktual.Likeok)
+                {
+                    aktual = max;
+                }
+
+
+            }
+            Console.WriteLine(max.Likeok);
+            Console.WriteLine("Van e olyan bejegyzés ami 35 nél több likeot kapott");
+            if (max.Likeok > 35)
+            {
+                Console.WriteLine("Igen van! Pontosan : {0}", max.Likeok);
+            }
+            else
+            {
+                Console.WriteLine("Nincs! A legtöbb likeot kapott száma:{0}", max.Likeok);
+            }
+            int kevesebbmint15 = 0;
+            for (int i = 0; i < lista.Count; i++)
+            {
+
+
+                if (lista[i].Likeok < 15)
+                {
+                    kevesebbmint15++;
+                }
+
+
+            }
+            Console.WriteLine("Összesen {0} bejegyzés kapott 15-nél kevesebb likeot", kevesebbmint15);
+
+            Bejegyzes swap;
+            for (int i = 0; i < lista.Count; i++)
+            {
+                for (int j = i + 1; j < lista.Count; j++)
+                {
+                    if (lista[j].Likeok > lista[i].Likeok)
+                    {
+                        swap = lista[i];
+                        lista[i] = lista[j];
+                        lista[j] = swap;
+                    }
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            for (int i = 0; i < lista.Count; i++)
+            {
+                Console.WriteLine(lista[i]);
+            }
+            StreamWriter sw = new StreamWriter("bejegyzesek_rendezett.txt");
+            for (int i = 0; i < lista.Count; i++)
+            {
+                sw.WriteLine(i);
+                sw.Write(Convert.ToString(lista[i].Szerzo));
+                sw.Write(lista[i].Tartalom);
+                sw.Write(lista[i].Likeok);
+                sw.Write(lista[i].Letrejot);
+                sw.Write(lista[i].Szerkesztve);
+
+            }
+            Console.ReadKey();
+
+
+
+
+
         }
     }
 }
